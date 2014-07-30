b3e4abdf8b151bab1fa3dbbde14cb5f6cb21e08d
dilflaer <dilflaer@gmail.com>
2014-07-30 21:04:07 +0400

    Создан выбор Мастер Игрока

--- src/nstuff/juggerfall/extension/MainExtension.java
+++ src/nstuff/juggerfall/extension/MainExtension.java
@@ -1,9 +1,36 @@
 package nstuff.juggerfall.extension;
 
+import com.smartfoxserver.v2.entities.User;
+import com.smartfoxserver.v2.entities.variables.SFSUserVariable;
+import com.smartfoxserver.v2.exceptions.SFSVariableException;
 import com.smartfoxserver.v2.extensions.ExtensionLogLevel;
 import com.smartfoxserver.v2.extensions.SFSExtension;
+import org.omg.CORBA.PUBLIC_MEMBER;
+
+import java.util.List;
 
 public class MainExtension extends SFSExtension {
+
+    public User masterInfo;
+
+    public void ChoiceMaster(User exMaster) throws SFSVariableException {
+
+        List<User> list = getParentRoom().getUserList();
+        if (list.size()!= 0){
+
+            for (int i=0; i<list.size(); i++){
+                User user = list.get(i);
+                if (user !=exMaster ){
+                    masterInfo = user;
+                    user.setVariable(new SFSUserVariable("Master",true ));
+                    return;
+                }
+
+
+            }
+
+        }
+    }
 
 	@Override
 	public void init() {
