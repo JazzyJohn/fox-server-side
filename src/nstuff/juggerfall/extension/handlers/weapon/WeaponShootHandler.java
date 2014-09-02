e5147b1f2ef813d169669b5b32f3d20a06de4c43
Ivan.Ochincenko <Ivan.Ochincenko@zed.com>
2014-09-02 18:20:55 +0400

    gun fix

--- src/nstuff/juggerfall/extension/handlers/weapon/WeaponShootHandler.java
+++ src/nstuff/juggerfall/extension/handlers/weapon/WeaponShootHandler.java
@@ -1,33 +1,44 @@
 package nstuff.juggerfall.extension.handlers.weapon;
 
 import com.smartfoxserver.v2.entities.User;
-import com.smartfoxserver.v2.entities.data.ISFSObject;
+import com.smartfoxserver.v2.entities.data.*;
 import com.smartfoxserver.v2.extensions.BaseClientRequestHandler;
 import nstuff.juggerfall.extension.MainExtension;
 import nstuff.juggerfall.extension.handlermanagers.WeaponHandlerManager;
 import nstuff.juggerfall.extension.pawn.Pawn;
 import nstuff.juggerfall.extension.weapon.Weapon;
 
+import java.util.Iterator;
+
 /**
  * Created by Ivan.Ochincenko on 30.07.14.
  */
 public class WeaponShootHandler  extends BaseClientRequestHandler {
     @Override
-    public void handleClientRequest(User user, ISFSObject data) {
-        Weapon weapon = (Weapon)((MainExtension)getParentExtension()).viewManager.getView(data.getInt("id"));
+    public void handleClientRequest(User user, ISFSObject alldata) {
+        ISFSArray shoots = alldata.getSFSArray("shoots");
+        ISFSArray verifyShoots = new SFSArray();
+        for(Iterator<SFSDataWrapper> iterator =shoots.iterator();iterator.hasNext();){
+            ISFSObject data = (ISFSObject) iterator.next().getObject();
 
-        if(weapon.owner==null){
-            Pawn owner  =(Pawn)((MainExtension)getParentExtension()).viewManager.getView(weapon.lateId);
-            if(owner!=null){
-                weapon.owner = owner;
-                owner.weapon = weapon;
+            Weapon weapon = (Weapon)((MainExtension)getParentExtension()).viewManager.getView(data.getInt("id"));
+
+            if(weapon.owner==null){
+                Pawn owner  =(Pawn)((MainExtension)getParentExtension()).viewManager.getView(weapon.lateId);
+                if(owner!=null){
+                    weapon.owner = owner;
+                    owner.weapon = weapon;
+                }
+            }else{
+                if(!weapon.owner.isOwner(user)){
+                    continue;
+                }
             }
-        }else{
-            if(!weapon.owner.isOwner(user)){
-                return;
-            }
+            verifyShoots.addSFSObject(data);
+
         }
-
-        send(WeaponHandlerManager.RequestName_WeaponShoot,data,((MainExtension)getParentExtension()).getOther(user));
+        ISFSObject verifyData = new SFSObject();
+        verifyData.putSFSArray("shoots",verifyShoots);
+        send(WeaponHandlerManager.RequestName_WeaponShoot,verifyData,((MainExtension)getParentExtension()).getOther(user));
     }
 }
